d52a754dfbd952ec3c7a8df6c0ec370b3cd36b8d
sachin yadav <sachinyadav63928@gmail.com>
2026-08-26 18:29:02 +0530

    new file

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.to(".box1", {
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"green",
+    borderRadius:"50%",
+    // scale:0.6
+})
+
+gsap.from(".box2", {
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"lightgreen",
+    borderRadius:"50%",
+    // scale:0.6
+})
