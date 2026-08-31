31c2f503f818c9757ac9ac1590f25081f74ad8f8
sachin yadav <sachinyadav63928@gmail.com>
2026-08-31 19:40:45 +0530

    add new file

---
+++ Text-Animations/script.js
@@ -0,0 +1,38 @@
+function breakTheText() {
+    let h1 = document.querySelector("h1")
+    let h1Text = h1.textContent
+
+    let splittedText = h1Text.split("")
+
+    let halfValue = splittedText.length/2
+
+    let clutter = ""
+
+    splittedText.forEach(function(elem,idx){
+        if(idx<halfValue) {
+            clutter += `<span class="a">${elem}</span>`
+        } else {
+            clutter += `<span class="b">${elem}</span>`
+        }
+    })
+
+    h1.innerHTML = clutter
+}
+
+breakTheText()
+
+gsap.from("h1 .a", {
+    y:80,
+    opacity:0,
+    duration:0.6,
+    delay:0.5,
+    stagger:0.15
+})
+
+gsap.from("h1 .b", {
+    y:80,
+    opacity:0,
+    duration:0.6,
+    delay:0.5,
+    stagger:-0.15
+})
